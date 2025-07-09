a0bf6bc7db6a692b0cf2cbf7d5e28498294ac7ff
Amit Bhardwaj <amitbhk2002@gmail.com>
2025-07-09 19:41:59 +0530

    add this file to make @Valid works

---
+++ backend/src/main/java/com/ecommerce/backend/controller/AuthController.java
@@ -0,0 +1,33 @@
+package com.ecommerce.backend.controller;
+
+import com.ecommerce.backend.model.User;
+import com.ecommerce.backend.repository.UserRepository;
+import com.ecommerce.backend.service.UserService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<String> signup(@Valid @RequestBody User user) {
+        return userService.saveNewUser(user);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@Valid @RequestBody User loginRequest) {
+        return userService.userLogin(loginRequest);
+    }
+}
