9202b9548bcca89f7f5ce2914a308ceaa7eb2c83
Amit Bhardwaj <amitbhk2002@gmail.com>
2025-07-09 22:33:31 +0530

    fix the cross origin url

--- backend/src/main/java/com/ecommerce/backend/controller/CategoryController.java
+++ backend/src/main/java/com/ecommerce/backend/controller/CategoryController.java
@@ -12,7 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/categories")
-@CrossOrigin(origins = "http://localhost:5173/")
+@CrossOrigin(origins = "http://localhost:5173")
 public class CategoryController {
 
     @Autowired
