392ecf2c5cbc6b35e594e55f898f605dd8c36728
Amit Bhardwaj <amitbhk2002@gmail.com>
2025-07-23 22:57:50 +0530

    add fullname property in user entity

--- backend/src/main/java/com/ecommerce/backend/model/User.java
+++ backend/src/main/java/com/ecommerce/backend/model/User.java
@@ -10,16 +10,16 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.UUID;
-
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
-    private UUID id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String fullname;
 
     @NotBlank(message = "Username is required")
     private String username;
--- backend/src/main/java/com/ecommerce/backend/repository/AuthRepository.java
+++ backend/src/main/java/com/ecommerce/backend/repository/AuthRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 import java.util.UUID;
 
-public interface AuthRepository extends JpaRepository<User, UUID> {
+public interface AuthRepository extends JpaRepository<User, Long> {
     public Optional<User> findByUsername(String username);
 }
