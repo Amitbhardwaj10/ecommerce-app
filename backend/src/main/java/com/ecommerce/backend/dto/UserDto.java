2b74946b2c915ee379618297b63e71a8ca85cdfe
Amit Bhardwaj <amitbhk2002@gmail.com>
2025-07-27 22:27:48 +0530

    create userDto

---
+++ backend/src/main/java/com/ecommerce/backend/dto/UserDto.java
@@ -0,0 +1,15 @@
+package com.ecommerce.backend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UserDto {
+    private String fullname;
+    private String username;
+}
