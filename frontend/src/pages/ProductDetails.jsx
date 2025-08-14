bff43c387e9ddfc70013ccecf25079bd49a42073
Amit Bhardwaj <amitbhk2002@gmail.com>
2025-08-15 02:20:10 +0530

    change in wishilist button

--- frontend/src/pages/ProductDetails.jsx
+++ frontend/src/pages/ProductDetails.jsx
@@ -27,7 +27,6 @@ function ProductDetails() {
 	const cartItems = useSelector((state) => state.cart.cartItems);
 	productId = Number(productId);
 	const { inWishlist, handleWishlistClick } = useWishlistActions(productId);
-
 	const inCart = cartItems.some((item) => item.productId == productId);
 
 	const getProdcutByProductId = async () => {
@@ -191,14 +190,20 @@ function ProductDetails() {
 								</svg>
 								{inCart ? "Go to cart" : "Add to cart"}
 							</button>
-							<button className="bg-gray-200 flex gap-2 items-center  text-gray-800 px-6 py-2 rounded-md hover:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2">
+							<button
+								className={`bg-gray-200 flex gap-2 items-center  text-gray-800 px-6 py-2 rounded-md hover:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2 ${
+									inWishlist && "bg-primary text-white"
+								}`}
+							>
 								<svg
 									xmlns="http://www.w3.org/2000/svg"
 									fill="none"
 									viewBox="0 0 24 24"
 									strokeWidth="1.5"
 									stroke="currentColor"
-									className="size-6"
+									className={`size-6 ${
+										inWishlist && "fill-rose-700 text-rose-700"
+									}`}
 									onClick={handleWishlistClick}
 								>
 									<path
@@ -207,7 +212,7 @@ function ProductDetails() {
 										d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z"
 									/>
 								</svg>
-								{inWishlist ? "Go to Wishlist" : "Add to Wishlist"}
+								{inWishlist ? "Wishlisted" : "Wishlist"}
 							</button>
 						</div>
 
