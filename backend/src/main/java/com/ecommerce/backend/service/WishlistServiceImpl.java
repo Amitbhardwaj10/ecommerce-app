0f9bb6a3b05c35a29c5b23b41341b72bcdda55bd
Amit Bhardwaj <amitbhk2002@gmail.com>
2025-08-21 23:32:25 +0530

    minor change

--- backend/src/main/java/com/ecommerce/backend/service/WishlistServiceImpl.java
+++ backend/src/main/java/com/ecommerce/backend/service/WishlistServiceImpl.java
@@ -12,7 +12,6 @@ import com.ecommerce.backend.repository.WishlistRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import javax.swing.text.html.Option;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
