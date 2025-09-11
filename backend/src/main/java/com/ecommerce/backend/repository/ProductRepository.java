7de8b826130c9a8fd293917a2da6e596622f3056
Amit Bhardwaj <amitbhk2002@gmail.com>
2025-09-12 01:43:23 +0530

    fix bug for lazy initialization exception

--- backend/src/main/java/com/ecommerce/backend/repository/ProductRepository.java
+++ backend/src/main/java/com/ecommerce/backend/repository/ProductRepository.java
@@ -2,22 +2,35 @@ package com.ecommerce.backend.repository;
 
 import com.ecommerce.backend.dto.MinMaxPrice;
 import com.ecommerce.backend.entity.Product;
+import lombok.NonNull;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long>, JpaSpecificationExecutor<Product> {
-    public List<Product> findByCategory_Id(Long id);
+    @EntityGraph(attributePaths = {"brand", "color", "category"})
+    List<Product> findByCategory_Id(Long id);
+
+    @EntityGraph(attributePaths = {"brand", "color", "category"})
+    @NonNull
+    List<Product> findAll();
+
+    @EntityGraph(attributePaths = {"brand", "color", "category"})
+    @NonNull
+    Optional<Product> findById( @NonNull Long id);
 
     @Query("SELECT DISTINCT p FROM Product p " +
             "JOIN FETCH p.brand " +
             "JOIN FETCH p.color " +
             "JOIN FETCH p.category")
     public List<Product> findAllWithRelations();
+
 
     @Query("SELECT MIN(p.price) as minPrice, MAX(p.price) as maxPrice FROM Product p")
     public MinMaxPrice findMinMaxPrice();
